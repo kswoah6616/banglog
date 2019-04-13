059413e135759b176f811829e365d0c257cfeedc
jm <kswoah123@naver.com>
2019-04-13 17:22:14 +0900

    [BO] 로그인 부분 생성 및 테스트

---
+++ application/views/admin/login/login.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>blog_ad</title>
+  </head>
+  <body>
+    <img src="images/firefox-icon.png" alt="My test image">
+  </body>
+</html>
