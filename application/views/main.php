f1a05a5621d3197c4b228fa175f7a29ac536017d
LeeYul <a24479276@gmail.com>
2019-04-14 17:14:19 +0900

    [fo]캐시수정

--- application/views/main.php
+++ application/views/main.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>welcom to banglog</title>
-    <link rel="stylesheet" href="/resource/css/common.css">
+    <link rel="stylesheet" href="/resource/css/common.css?ver=1.1">
   </head>
   <BODY>
     <div class="top">
