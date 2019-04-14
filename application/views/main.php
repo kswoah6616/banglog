ab435216a7e2e6ab28f96f718ac24d1c5fce9af9
jm <kswoah123@naver.com>
2019-04-14 17:52:52 +0900

    [FO]캐시 수정

--- application/views/main.php
+++ application/views/main.php
@@ -3,11 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>welcom to banglog</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="/resource/css/common.css?ver=<?= RES_VER?>">
-=======
-    <link rel="stylesheet" href="/resource/css/common.css?ver=1.2">
->>>>>>> 617ed6fbefb96fd464d00d50c935b729d9d0288a
   </head>
   <body>
     <div class="top"></div>
