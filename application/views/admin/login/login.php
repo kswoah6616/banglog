ee58e906eb4e8ce9cd5bbd975c8b7620e5dbb466
jm <kswoah123@naver.com>
2019-04-17 22:05:05 +0900

    [BO]관리자 페이지

--- application/views/admin/login/login.php
+++ application/views/admin/login/login.php
@@ -6,22 +6,19 @@
     <link rel="stylesheet" href="/resourceadmin/css/login.css?ver=<?= RES_VER?>">
   </head>
   <body>
-    <div class='login_background'>
-      <div>
-        <span class="login_title">Banglog</span>
-        <br>
-        <div class="form_box">
-          아이디 : <input type='text' />
-          <br>
-          비밀번호 : <input type='text' />
-          <br>
-          ID 저장<input type="radio"/>
-          <br>
-          <br>
-          <input type="button" value="로그인"/>
-        </div>
-        
+    <div class="login-box">
+      <h1>Bang's</h1>
+      <div class="textbox">
+        <i class="fas fa-user"></i>
+        <input type="text" placeholder="Username">
       </div>
+
+      <div class="textbox">
+        <i class="fas fa-lock"></i>
+        <input type="password" placeholder="Password">
+      </div>
+
+      <input type="button" class="btn" value="Sign in">
     </div>
   </body>
 </html>
