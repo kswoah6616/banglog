1e6fc56dc3e9df708d290f3e32072417a5119bd4
jm <kswoah123@naver.com>
2019-04-20 17:03:23 +0900

    [BO] js 경로수정

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -3,13 +3,19 @@
   <head>
     <meta charset="utf-8">
     <title>welcom to banglog</title>
+
+    <!-- css -->
     <link rel="stylesheet" href="/resourceadmin/css/common.css?ver=<?= RES_VER?>">
+
+    <!-- js -->
+    <script src="/resourceadmin/main/main.js"></script>
   </head>
   <body>
     <div class="top" style="font-size:50px; background-color:skyblue;">
       관리자 메인 페이지 입니다. 개발중~~~~~~
     </div>
-    
+    <div style="border:1px solid black; background-color:black;">꿀떼지</div>
+    <input class='btnRed' type='button' value='이거 눌러봐라'>
 
   
 </body>
