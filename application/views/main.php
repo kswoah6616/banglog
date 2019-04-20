23d777a95ecdfeb8c1c683bdb450a2407281b4e9
LeeYul <a24479276@gmail.com>
2019-04-20 16:13:49 +0900

    [fo]상단메뉴

--- application/views/main.php
+++ application/views/main.php
@@ -2,19 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>welcom to banglog</title>
+    <title>YouTube</title>
     <link rel="stylesheet" href="/resource/css/common.css?ver=<?= RES_VER?>">
   </head>
   <body>
     <div class="top"></div>
-    <div class="center"></div>
-    <div class="a-1"></div>
-    <div class="a-2"></div>
-    <div class="a-2"></div>
-    <div class="a-2"></div>
-    <div class="a-2"></div>
- 
-
+    <div class="search"></div>
+    
   
 </body>
 
