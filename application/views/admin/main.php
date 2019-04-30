bf2e330b045a6763583cde203784342c05b857f5
jm <kswoah123@naver.com>
2019-04-30 23:35:19 +0900

    [BO] main 세션 나누기 수정

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -14,6 +14,10 @@
   <body>
     <div class="side_menu">
       <div class="admin_icon">아이콘입니다</div>
+      <div class="admin_menu">메뉴1</div>
+      <div class="admin_menu">메뉴2</div>
+      <div class="admin_menu">메뉴3</div>
+      <div class="admin_menu">메뉴4</div>
     </div>
     <div class="top_menu">탑메뉴입니다.</div>
     <div class="header">헤더입니다.</div>
