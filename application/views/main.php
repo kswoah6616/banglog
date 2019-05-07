b8b823c8bdad43d2cc44430c24cfa1443527fd6f
LeeYul <a24479276@gmail.com>
2019-05-07 18:01:50 +0900

    [fo]상단메뉴

--- application/views/main.php
+++ application/views/main.php
@@ -2,22 +2,35 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>YouTube</title>
+    <title>Bike</title>
     <link rel="stylesheet" href="/resource/css/common.css?ver=<?= RES_VER?>">
-
+    <link rel="shortcut icon" type="image/x-icon" href="resource/image/main/bike.ico">
     <!-- js -->
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="/resource/js/main/main.js"></script>
-
-
+    
   </head>
-  <body>
-  <form action= "/admin/main/index" mathod= "post" accept-charset= "utf-8"  name= "test">
-    <input type= "text" name= "ss">
-    <div id="motion">앙떼지</div>
-  </form>
-    <input id="btn" type="button" value="이거 눌러봐라">
-  
-</body>
 
-</HTML> 
+
+  <body>
+  <!-- main -->
+  <form action= "/admin/main/index" mathod= "post" accept-charset= "utf-8"  name= "test">
+  <div class="search_box">
+    <div class="container_1">
+      <span class="main_search"><i class="main_search"></i></span>
+      <input class="search" type="search" placeholder="Search...">
+    </div> 
+</div>
+    </form>
+  
+  
+  <!-- <input class="search_btn" type="button"> -->
+  <!-- <div class="top_menu"></div> -->
+  <!-- <input type= "text" name="search_bar"> -->
+  
+  
+  
+
+  </body>
+  </HTML> 
+  
