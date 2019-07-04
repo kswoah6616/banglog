ba850f70f82962a582e3fe4aac36534fddb5c98e
LeeYul <a24479276@gmail.com>
2019-07-04 19:12:00 +0900

    [fo]상단메뉴

--- application/views/main.php
+++ application/views/main.php
@@ -15,27 +15,23 @@
   <body>
   <!-- main_Top-->
   <div class = "top_menu">
-    <!-- <form action= "/admin/main/index" mathod= "post" accept-charset= "utf-8"  name= "test">
+    <form action= "/admin/main/index" mathod= "post" accept-charset= "utf-8"  name= "test">
       <div class="search_box">
         <input type="text" placeholder="Search..." name="search">
         <input type="button" value="검색" onclick="alert(ok)"> 
       </div>
-    </form> -->
+    </form>
   </div>
   
-
   <!--right menu-->
 
   <!--left menu-->
   <a class="logo" href="#" taget="_self"><img src="resource/image/main/motorcycle.jpg" alt="BIKE"></a>
 
-
   <!--center view-->
 
   <!--bottom-->
   
-
-
   </body>
   </HTML> 
   
