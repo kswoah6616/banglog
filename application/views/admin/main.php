9be5f4295af9e50b69ad8b245e87f1fa3852289d
jm <kswoah123@naver.com>
2019-07-13 19:23:17 +0900

    [FO] 관리자 메인 틀잡음

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Bike</title>
-    <link rel="stylesheet" href="/resource/css/common.css?ver=<?= RES_VER?>">
+    <link rel="stylesheet" href="/resourceadmin/css/common.css?ver=<?= RES_VER?>">
     <link rel="shortcut icon" type="image/x-icon" href="resource/image/main/bike.ico">
     <!-- js -->
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
@@ -12,8 +12,37 @@
   </head>
 
   <body>
-왕꿀뗴지
-  
+    <div class="fullTag">
+      <!--탑 메뉴 -->
+      <div class="top_menu">
+          <div>땡떙땡관리자님<span>로그아웃</span></div>
+      </div>
+      <!--//탑 메뉴 -->
+
+      <!--사이드 메뉴 -->
+      <div class="side_menu">
+          
+      </div>
+      <!--//사이드 메뉴 -->
+
+      <!--헤더 메뉴 -->
+      <div class="header">
+          
+      </div>
+      <!--//헤더 메뉴 -->
+
+      <!--본문 메뉴 -->
+      <div class="content">
+          
+      </div>
+      <!--//본문 메뉴 -->
+
+      <!--푸터 메뉴 -->
+      <div class="footer">
+      
+      </div>
+      <!--//푸터 메뉴 -->
+    </div>
   </body>
-  </HTML> 
+ </HTML> 
   
