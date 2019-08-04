62f102abe2ac0432ccf40cc4c3f7b3ad2c6d78e7
jm <kswoah123@naver.com>
2019-08-04 21:38:41 +0900

    [BO] 프로필사진생성

--- application/views/admin/common/side_menu.php
+++ application/views/admin/common/side_menu.php
@@ -1,30 +1,10 @@
    <!--사이드 메뉴 -->
    <div class="side_menu">
-      <div class="menu">
-      <li class="item">
-         <div class="accordion_btn"><i class="far fa-user"></i>방문자 관리</div>
-         <div class="smenu">
-            <a href="#">Posts</a>
-            <a href="#">Picture</a>
+      <!-- 프로필 -->
+      <div class="profile">
+         <div class="profile_img">
+            <img class="img_circle" src="/resourceadmin/image/no_detail_img.gif" alt="프로필 이미지">
          </div>
-      </li>
-
-      <li class="item">
-         <div class="accordion_btn"><i class="far fa-envelope"></i>게시글 관리</div>
-         <div class="smenu">
-            <a href="#">new</a>
-            <a href="#">Sent</a>
-            <a href="#">Spam</a>
-         </div>
-      </li>
-
-      <li class="item">
-         <div class="accordion_btn"><i class="fas fa-cog"></i>접촉 문의</div>
-         <div class="smenu">
-            <a href="#">Password</a>
-            <a href="#">Language</a>
-         </div>
-      </li>
       </div>
    </div>    
     <!--//사이드 메뉴 -->
