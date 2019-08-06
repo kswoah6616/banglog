50a89a50fcbaefa9207d63897b055be750c503d0
jm <kswoah123@naver.com>
2019-08-06 23:09:09 +0900

    [BO] 프로필 사진 생성 원형 개발

--- resourceadmin/js/common/css_common.js
+++ resourceadmin/js/common/css_common.js
@@ -1,10 +1,5 @@
 $(function(){
 
-    //아코디언 버튼 이벤트
-    $('.accordion_btn').on('click', function(e){
-        $('.accordion_btn').removeClass('on');
-        $(this).addClass('on');
-        $(this).parent().find('.smenu').show();
-    })
+   
 
 });
